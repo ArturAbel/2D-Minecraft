17d33bc468e7f5796d21c9e41c9a832e4947ab1d
Artur Abel <163734294+ArturAbel@users.noreply.github.com>
2024-05-25 23:23:53 +0300

    worked on tools and the mineCraft method

--- modules/intro.js
+++ modules/intro.js
@@ -4,6 +4,7 @@ import {
   introTitle,
   startButton,
   resetButton,
+  quitButton,
 } from "./elements.js";
 
 export class IntroModule {
@@ -12,10 +13,18 @@ export class IntroModule {
     this.introTitle = introTitle;
     this.startButton = startButton;
     this.resetButton = resetButton;
+    this.quitButton = quitButton;
   }
+
+  quit(){
+    this.quitButton.addEventListener('click', () => alert(`Not yet ready!`));
+  }
+
   init() {
     this.startButton.addEventListener("click", () => this.startGame());
   }
+
+
 
   startGame() {
     this.hideIntroContainer();
